e2e29f7e88ab6d38f2b6081a1f992482d890e04c
AdvaitThavare <advait.thavare241995@gmail.com>
2026-09-23 09:10:15 +0530

    Update regression tests for client authentication

--- PlaywrightSuite/apiSetup/clientCryptoHelper/helper-clientJWSAESRSA.ts
+++ PlaywrightSuite/apiSetup/clientCryptoHelper/helper-clientJWSAESRSA.ts
@@ -20,6 +20,8 @@ export async function encryptClientJWSAESRSA(
         '/clientCryptography/encryptJWS_AES_RSA',
         {
             headers: {
+                'x-at-client-id': 'AT-CLIENT-001',
+                'x-at-client-secret': 'AT-SECRET-001',
                 'x-payload-state': 'PLAIN',
                 'x-data-encryption': 'NA',
                 'content-type': contentType,
@@ -47,6 +49,8 @@ export async function decryptClientJWSAESRSA(
         '/clientCryptography/decryptJWS_AES_RSA',
         {
             headers: {
+                'x-at-client-id': 'AT-CLIENT-001',
+                'x-at-client-secret': 'AT-SECRET-001',
                 'x-payload-state': 'PLAIN',
                 'x-data-encryption': 'NA',
                 'content-type': 'application/json',
